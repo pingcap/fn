f7fc8fa0bffaf6d6a9b131d500176268874675de
zeromake <a390720046@gmail.com>
2020-03-06 11:27:21 +0800

    fix: refactoring code by code review

--- error_test.go
+++ error_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 PingCAP, Inc.
+// Copyright 2020 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,9 +15,10 @@ package fn
 
 import (
 	"errors"
-	. "github.com/pingcap/check"
 	"net/http"
 	"testing"
+
+	. "github.com/pingcap/check"
 )
 
 var (
