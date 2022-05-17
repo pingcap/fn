72e08d03ca1e4ffe93019fa9fff6babee2a1779a
tison <wander4096@gmail.com>
2022-05-17 12:21:03 +0800

    Update license header
    Signed-off-by: tison <wander4096@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/pingcap/fn
 
 go 1.11
 
-require github.com/pingcap/check v0.0.0-20190102082844-67f458068fc8
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pingcap/check v0.0.0-20190102082844-67f458068fc8
+	github.com/stretchr/testify v1.7.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20220512140231-539c8e751b99 // indirect
+)
--- pre_go113.go
+++ pre_go113.go
@@ -1,4 +1,5 @@
-//+build !go1.13
+//go:build !go1.13
+// +build !go1.13
 
 // Copyright 2020 PingCAP, Inc.
 //
@@ -10,6 +11,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
